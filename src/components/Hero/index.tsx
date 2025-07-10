03f830cf868aa362dc27e396d2df31ed6cc95e8d
Gabriel <gabriel_lopes355@yahoo.com.br>
2025-07-10 13:49:43 -0300

    teste do bug do modo dark

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,16 +6,16 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative z-10 overflow-hidden w-full pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="relative z-10 overflow-hidden w-full pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   Transform Your Florida Space: Premium Remodeling, Start to Finish.
                 </h1>
-                <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                <p className="mb-12 text-base leading-relaxed! text-body-color sm:text-lg md:text-xl">
                   Experts in Demolition, Kitchens, Bathrooms, and Flooring. Quality and Trust You Can See.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
