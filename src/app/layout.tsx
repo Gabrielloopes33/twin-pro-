4a660a1f6c24ea873282191f92d010a4c42dbece
Gabriel <gabriel_lopes355@yahoo.com.br>
2026-03-20 08:19:19 -0300

    feat: add GA4 tracking tag (G-LW9ER34826) and consolidate gtag scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,14 @@ export default function RootLayout({
       */}
       <head>
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10833965670"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LW9ER34826"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+              gtag('config', 'G-LW9ER34826');
               gtag('config', 'AW-10833965670');
             `,
           }}
